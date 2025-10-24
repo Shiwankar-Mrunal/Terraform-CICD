cae3f40262a88397b74b08f20a8eceb8fca88d24
Shiwankar-Mrunal <Mrunal.Shiwankar@engelglobal.com>
2025-10-24 14:08:08 +0530

    first commit

--- ci-app/test_app.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from app import app
-
-def test_hello():
-    tester = app.test_client()
-    response = tester.get("/")
-    assert response.status_code == 200
-    assert b"Hello from Azure Web App!" in response.data
-    
